e90ae98c34cc4189ebe4f82a52fe4b6a2dfa796e
hubert <hubert.rybicki.hr@gmail.com>
2023-05-07 22:16:18 +0200

    Dodanie metody do szukania numeru pierwszego wolnego slotu

--- src/main/java/pl/pomoku/pomokupluginsrepository/players/InventorySystem.java
+++ src/main/java/pl/pomoku/pomokupluginsrepository/players/InventorySystem.java
@@ -3,12 +3,21 @@ package pl.pomoku.pomokupluginsrepository.players;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
 @SuppressWarnings("unused")
 public class InventorySystem {
-    public static boolean oneFreeSlot(Player player){
-        for(ItemStack item : player.getInventory()){
-            if(item == null || item.getType() == Material.AIR) return true;
+    public static boolean oneFreeSlot(Player player) {
+        for (ItemStack item : player.getInventory()) {
+            if (item == null || item.getType() == Material.AIR) return true;
         }
         return false;
+    }
+
+    public static Integer firstFreeSlot(Player player) {
+        for (int i = 0; i < player.getInventory().getSize(); i++) {
+            ItemStack item = player.getInventory().getItem(i);
+            if (item == null || item.getType() == Material.AIR) return i;
+        }
+        return null;
     }
 }
